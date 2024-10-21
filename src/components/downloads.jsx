16e7a84f357e184946130c4ef48e39d4e6b97f52
Ranjula Ilukpitiya <ranjulailukpitiya@gmail.com>
2024-10-21 20:34:26 +0530

    Final changes

--- src/components/downloads.jsx
+++ src/components/downloads.jsx
@@ -9,25 +9,21 @@ import { motion } from "framer-motion";
 
 const Downloads = () => {
   const pdfFiles = [
-    { name: "Proposal_Report_24_IT21002106", url: "/pdf/Proposal_Report_24_IT21002106.pdf" },
-    { name: "Proposal_Report_24_IT21011016", url: "/pdf/Proposal_Report_24_IT21011016.pdf" },
-    { name: "Proposal_Report_24_IT21114144", url: "/pdf/Proposal_Report_24_IT21114144.pdf" },
-    { name: "Proposal_Report_24_IT21128936", url: "/pdf/Proposal_Report_24_IT21128936.pdf" },
-    { name: "Status_Document_01_24-IT21002106", url: "/pdf/Status_Document_01_24-IT21002106.pdf" },
-    { name: "Status_Document_01_24-IT21011016", url: "/pdf/Status_Document_01_24-IT21011016.pdf" },
-    { name: "Status_Document_01_24-IT21114144", url: "/pdf/Status_Document_01_24-IT21114144.pdf" },
-    { name: "Status_Document_01_24-IT21128936", url: "/pdf/Status_Document_01_24-IT21128936.pdf" },
-    { name: "Status_Document_02_24-IT21114144", url: "/pdf/Status_Document_02_24-IT21114144.pdf" },
-    { name: "Status_Document_02_24-IT21128936", url: "/pdf/Status_Document_02_24-IT21128936.pdf" },
-    { name: "Status_Document_02_24-IT21002106", url: "/pdf/Status_Document_02_24-IT21002106.pdf" },
-    { name: "Status_Document_02_24-IT21011016", url: "/pdf/Status_Document_02_24-IT21011016.pdf" },
+    { name: "Topic Assessment", type1: "Group", type2: "", date: "Submitted Date - 2024/01/18", url: "https://drive.google.com/file/d/1jnbRLTUTtMjWmJla-RbCdI0xhxkEyKgq/view?usp=sharing", devider:"" },
+    { name: "Project Charter", type1: "Group", type2: "", date: "Submitted Date - 2024/02/19", url: "", devider:"" },
+    { name: "Project Proposal", type1: "", type2: "Individual", date: "Submitted Date - 2024/02/29", url: "https://drive.google.com/drive/folders/1J6eRSFM-b3tde3tlmsRkRz1NQSm2yiSk?usp=sharing", devider:"" },
+    { name: "Status Document I", type1: "", type2: "Individual", date: "Submitted Date - 2024/05/06", url: "https://drive.google.com/drive/folders/1LDWp8nckV8GeSbx_CaV0IULqqQKE5zym?usp=sharing", devider:"" },
+    { name: "Status Document II", type1: "", type2: "Individual", date: "Submitted Date - 2024/09/11", url: "https://drive.google.com/drive/folders/14wuk0iPVqg1of3BjJ28Xw9FI10ZTbSvJ?usp=sharing", devider:"" },
+    { name: "Research Paper", type1: "Group", type2: "", date: "Submitted Date - 2024/06/14", url: "https://drive.google.com/drive/folders/1wr_wiJngX3LssG7ay49C2nQ2ZyrQUZO6?usp=sharing", devider:"" },
+    { name: "Final Report", type1: "Group", type2: "Individual", date: "Submitted Date - 2024/08/23", url: "https://drive.google.com/drive/folders/1V3ovkX4ZxKnSJJItRtLupPT0L8zC56ep?usp=sharing" , devider:"/"},
+    { name: "Poster", type1: "Group", type2: "", date: "Submitted Date - 2024/10/13", url: "https://drive.google.com/drive/folders/1K5LnG-3HdMnaCJVZwamgF6ynW0nvYuVF?usp=sharing" },
   ];
 
   const pptFiles = [
-    { name: "Proposal Presentation", url: "/ppt/ProposalPresentation.pptx" },
-    { name: "Progress Presentation I", url: "/ppt/ProgressPresentation1.pptx" },
-    { name: "Progress Presentation II", url: "/ppt/Progress Presentation2.pptx" },
-    { name: "Final Presentation", url: "" },
+    { name: "Proposal Presentation", url: "https://docs.google.com/presentation/d/1f5-KlmuA21KESFRTQNcF20wsfRPNxaFS/edit?usp=sharing" },
+    { name: "Progress Presentation I", url: "https://docs.google.com/presentation/d/1xMSs-cMwa_aiY--tGKN6qGkOB77csHbm/edit?usp=sharing" },
+    { name: "Progress Presentation II", url: "https://docs.google.com/presentation/d/1d_vESQWf30H-DfLQQdvXVCI83g-x8IJY/edit?usp=sharing" },
+    { name: "Final Presentation", url: "https://drive.google.com/drive/folders/1_48z-xRBVdO_3QFwnG46Wg6R65AA9LSt?usp=sharing" },
   ];
 
   const containerVariants = {
@@ -44,43 +40,47 @@ const Downloads = () => {
   };
 
   return (
-    <div className="downloads-section bg-black text-white py-10 px-60" id="Downloads">
-     
+    <div className="downloads-section bg-black text-white py-10 px-4 md:px-20 lg:px-60" id="Downloads">
+      {/* Title and Description */}
       <motion.p
-          className="text-xl text-start mb-2 text-[#FF9500]"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7 }}
-        >Downloads</motion.p>
+        className="text-xl text-start mb-2 text-[#FF9500]"
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        Downloads
+      </motion.p>
       
       <motion.p
-          className="text-4xl font-bold text-start"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7 }}
-        >Documents</motion.p>
+        className="text-4xl font-bold text-start"
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        Documents
+      </motion.p>
       
       <motion.p
-          className=" text-start mb-8"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7 }}
-        >Please find all documents related to this project below.</motion.p>
+        className="text-start mb-8"
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        Please find all documents related to this project below.
+      </motion.p>
 
-      {/* PDFs */}
-
+      {/* PDFs Section */}
       <div>
         <motion.div
           className="pdf-section grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6"
           variants={containerVariants}
-          // initial="hidden"
           animate="visible"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.2 }}
         >
           {pdfFiles.map((file, index) => (
-            <motion.div key={index} variants={itemVariants} >
+            <motion.div key={index} variants={itemVariants}>
               <Card
                 className="shadow-lg hover:shadow-2xl hover:animate-pulse transition duration-300"
                 hoverable
@@ -90,13 +90,15 @@ const Downloads = () => {
                       fontSize: "40px",
                       color: "#f5222d",
                       marginTop: "4px",
+                      textAlign: "center",
                     }}
                   />
                 }
                 actions={[
                   <a
                     href={file.url}
-                    download={file.name}
+                    target="_blank"  // Opens link in new tab
+                    rel="noopener noreferrer"  // Security enhancement
                     className="flex justify-center"
                   >
                     <DownloadOutlined /> Download
@@ -104,33 +106,43 @@ const Downloads = () => {
                 ]}
               >
                 <Card.Meta title={file.name} />
+                <div className="mt-2 text-sm flex">
+                  <div className="text-blue-600">{file.type1}</div>
+                  <div className="text-gray-400">{file.devider}</div>
+                  <div className="text-green-500">{file.type2 && `${file.type2}`}</div>
+                </div>
+                <p className="mt-2 text-gray-400 text-sm">
+                {file.date}
+                </p>
               </Card>
             </motion.div>
           ))}
         </motion.div>
       </div>
 
-      {/* PPTs */}
+      {/* PPT Section */}
+      <motion.p
+        className="text-4xl font-bold text-start mt-16"
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        Presentations
+      </motion.p>
       
       <motion.p
-          className="text-4xl font-bold text-start mt-16"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7 }}
-        >Presentations</motion.p>
-      
-      <motion.p
-          className=" text-start mb-8"
-          initial={{ opacity: 0, y: 20 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7 }}
-        >Please find all presentations related this project below.</motion.p>
+        className="text-start mb-8"
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        Please find all presentations related to this project below.
+      </motion.p>
 
       <div>
         <motion.div
           className="ppt-section grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-10"
           variants={containerVariants}
-          // initial="hidden"
           animate="visible"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -147,13 +159,15 @@ const Downloads = () => {
                       fontSize: "40px",
                       color: "#fa8c16",
                       marginTop: "4px",
+                      textAlign: "center",
                     }}
                   />
                 }
                 actions={[
                   <a
                     href={file.url}
-                    download={file.name}
+                    target="_blank"  // Opens link in new tab
+                    rel="noopener noreferrer"  // Security enhancement
                     className="flex justify-center"
                   >
                     <DownloadOutlined /> Download
